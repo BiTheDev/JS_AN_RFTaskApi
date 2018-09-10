e36d70f838a664d3a02f346767a5ef401325b112
BrianHo <Curry30@Brians-MacBook-Pro.local>
2018-09-10 15:34:42 -0700

    error and success msg

--- server/controller.js
+++ server/controller.js
@@ -9,20 +9,20 @@ const Tasks = require("./model.js");
 
 module.exports = {
     home : (req,res) => Tasks.find({})
-                        .then(data => console.log("root error")||res.json(data))
-                        .catch(errs => console.log("root success")||res.json(errs)),
+                        .then(data => console.log("root success")||res.json(data))
+                        .catch(errs => console.log("root error")||res.json(errs)),
     getTask : (req,res) => Tasks.find({_id : req.params.id})
-                            .then(data => console.log("getTask error")||res.json(data))
-                            .catch(errs => console.log("getTask success")||res.json(errs)),
+                            .then(data => console.log("getTask success")||res.json(data))
+                            .catch(errs => console.log("getTask error")||res.json(errs)),
     createTask : (req,res) => Tasks.create(req.body)
-                                .then(data => console.log("createTask error")||res.json(data))
-                                .catch(errs => console.log("createTask success")||res.json(errs)),
+                                .then(data => console.log("createTask success")||res.json(data))
+                                .catch(errs => console.log("createTask error")||res.json(errs)),
     updateTask : (req,res) => Tasks.update({_id: req.params.id},{$set : req.body})
-                                .then(data => console.log("updateTask error")||res.json(data))
-                                .catch(errs => console.log("updateTask success")||res.json(errs)),
+                                .then(data => console.log("updateTask success")||res.json(data))
+                                .catch(errs => console.log("updateTask error")||res.json(errs)),
     removeTask : (req,res) => Tasks.remove({_id : req.params.id})
-                                .then(data => console.log("removeTask error")||res.json(data))
-                                .catch(errs => console.log("removeTask success")||res.json(errs))
+                                .then(data => console.log("removeTask success")||res.json(data))
+                                .catch(errs => console.log("removeTask error")||res.json(errs))
                                 
 }
 
